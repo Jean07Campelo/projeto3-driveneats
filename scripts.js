ca318356e61df66c1fe810d3e65c9d162b200028
Jean <jean93campelo@gmail.com>
2022-04-03 16:42:06 -0300

    selecao de um item em cada categoria

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 
+/*PRATO PRINCIPAL*/
 function selecionarPrato (prato) {
     /*BUSCA ITEM SELECIONADO*/
-    const itemSelecionado = document.querySelector(".selecionado");
+    const itemSelecionado = document.querySelector(".pratos").querySelector(".selecionado");
     if (itemSelecionado !== null) {
         itemSelecionado.classList.remove("selecionado");
     }
@@ -9,12 +10,26 @@ function selecionarPrato (prato) {
     prato.classList.add("selecionado");
 }
 
+/*BEBIDAS*/
 function selecionarBebida (bebida) {
     /*BUSCA ITEM SELECIONADO*/
-    const itemSelecionado = document.querySelector(".selecionado");
+    const itemSelecionado = document.querySelector(".bebidas").querySelector(".selecionado");
     if (itemSelecionado !== null) {
         itemSelecionado.classList.remove("selecionado");
     }
     /*ADICIONA CLASSE SELECIONADO NO ELEMENTO CLICADO*/
     bebida.classList.add("selecionado");
 }
+
+/*SOBREMESA*/
+function selecionarSobremesa (sobremesa) {
+    /*BUSCA ITEM SELECIONADO*/
+    const itemSelecionado = document.querySelector(".sobremesas").querySelector(".selecionado");
+    if (itemSelecionado !== null) {
+        itemSelecionado.classList.remove("selecionado");
+    }
+    /*ADICIONA CLASSE SELECIONADO NO ELEMENTO CLICADO*/
+    sobremesa.classList.add("selecionado");
+}
+
+

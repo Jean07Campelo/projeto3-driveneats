49b6222c16c15aff4dce9c5376d5d2febe870d32
Jean <jean93campelo@gmail.com>
2022-04-04 09:48:12 -0300

    configuracao icone em itens selecionados

--- scripts.js
+++ scripts.js
@@ -1,35 +1,63 @@
+let pratoSelecionado;
+let valorPrato;
+let bebidaSelecionada;
+let valorBebida;
+let sobremesaSelecionada;
+let valorSobremesa;
 
-/*PRATO PRINCIPAL*/
+
+//PRATO PRINCIPAL
 function selecionarPrato (prato) {
     /*BUSCA ITEM SELECIONADO*/
     const itemSelecionado = document.querySelector(".pratos").querySelector(".selecionado");
     if (itemSelecionado !== null) {
         itemSelecionado.classList.remove("selecionado");
+        itemSelecionado.querySelector(".icone-selecionado").classList.add("esconder");
+        
     }
     /*ADICIONA CLASSE SELECIONADO NO ELEMENTO CLICADO*/
     prato.classList.add("selecionado");
+    prato.querySelector(".icone-selecionado").classList.remove("esconder");
+    
+    pratoSelecionado = document.querySelector(".pratos").querySelector(".selecionado").querySelector(".nome-item").innerHTML;
+    valorPrato = document.querySelector(".pratos").querySelector(".selecionado").querySelector(".valor").innerHTML;    
 }
 
-/*BEBIDAS*/
+//BEBIDAS
 function selecionarBebida (bebida) {
     /*BUSCA ITEM SELECIONADO*/
     const itemSelecionado = document.querySelector(".bebidas").querySelector(".selecionado");
     if (itemSelecionado !== null) {
         itemSelecionado.classList.remove("selecionado");
+        itemSelecionado.querySelector(".icone-selecionado").classList.add("esconder");
     }
     /*ADICIONA CLASSE SELECIONADO NO ELEMENTO CLICADO*/
     bebida.classList.add("selecionado");
+    bebida.querySelector(".icone-selecionado").classList.remove("esconder");
+
+    bebidaSelecionada = document.querySelector(".pratos").querySelector(".selecionado").querySelector(".nome-item").innerHTML;
+    valorBebida = document.querySelector(".pratos").querySelector(".selecionado").querySelector(".valor").innerHTML;
 }
 
-/*SOBREMESA*/
+//SOBREMESA
 function selecionarSobremesa (sobremesa) {
     /*BUSCA ITEM SELECIONADO*/
     const itemSelecionado = document.querySelector(".sobremesas").querySelector(".selecionado");
     if (itemSelecionado !== null) {
         itemSelecionado.classList.remove("selecionado");
+        itemSelecionado.querySelector(".icone-selecionado").classList.add("esconder");
     }
     /*ADICIONA CLASSE SELECIONADO NO ELEMENTO CLICADO*/
     sobremesa.classList.add("selecionado");
+    sobremesa.querySelector(".icone-selecionado").classList.remove("esconder");
+
+    sobremesaSelecionada = document.querySelector(".pratos").querySelector(".selecionado").querySelector(".nome-item").innerHTML;
+    valorSobremesa = document.querySelector(".pratos").querySelector(".selecionado").querySelector(".valor").innerHTML;
 }
 
+function tudoSelecionado () {
+    if (".pratos" && ".bebidas" && ".sobremesas" !== undefined) {
+        document.querySelector(".escolhendo-pedido").classList.add("esconder");
+    }
+}
 
